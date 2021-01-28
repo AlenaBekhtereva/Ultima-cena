0a51d8258bcce731e8e5af25cee8611cb5c75872
AlenaBekhtereva <71527780+AlenaBekhtereva@users.noreply.github.com>
2021-01-28 20:12:56 +0300

    Add files via upload

---
+++ symbol.php
@@ -0,0 +1,82 @@
+<html>
+<head>
+<style>
+	.smalltext{
+		font-family:  "Century Gothic", serif;
+		font-style: normal;
+		font-weight: 800
+		font-stretch: expanded;
+		padding: 20px;
+	}
+	.bigtext{
+		font-family:  "Century Gothic", serif;
+		font-style: bold;
+		font-size: xx-large;
+		font-weight: 800
+		font-stretch: expanded;
+		margin-bottom:20px;
+	}
+	.knopka {
+		color: #75675e;
+		text-decoration: none;
+		user-select: none;
+		background: #fff ; 
+		padding: .7em 1.5em; 
+		outline: none; 
+	}
+</style>
+</head>
+<body>
+<table align = center  width = 800px>
+	<tr height=60px bgcolor=#75675e>
+		<td valign="bottom">
+		<form name=back action=index.php method=POST ></br>
+			<input type="submit" name"Back" value="Вернуться" class="knopka"/>
+		</form>
+		</td>
+	</tr>
+	<tr height = 30% align =center>
+		<td align=center>
+			<div>
+				<font class="bigtext">Символика фрески</font></br>
+			</div>
+			<div align=left >
+				<font class="smalltext">Ряд искусствоведов и ученых активно обсуждают значение сосуда с разлитой солью возле локтя Иуды. Пролитая соль может символизировать неудачу, потерю религии или веры в Христа. Вторая символическая загадка: является ли рыба на столе сельдью или угрем. Это важно, поскольку у каждого свое символическое значение. По—итальянски слово «угорь» — «аринга» означает внушение. На северно—итальянском диалекте слово «сельдь» — «ренга» описывает человека, который отрицает религию (и это соответствовует библейскому предсказанию Иисуса о том, что его апостол Петр будет отрицать, что знает его). Таким образом, угорь — символизирует веру в Иисуса, а сельдь, напротив, символизирует неверующего.</font></br>
+			</div>
+		</td>
+	</tr>
+	<tr>
+		<td width = 1100px align=center>
+			<div>
+			<img src=символика.png alt="символика" > </br>
+			</div>
+		</td>
+	</tr>
+</table>
+<table align=center width=900px>
+	<tr height=60% width=100%>
+		<td align=left>
+		<div align=left width=450px>
+			<font class="smalltext">1. Пролитая соль возле Иуды символизирует неудачу, потерю религии или веры в Христа.</font></br>
+			<font class="smalltext">2. В руке Иуды мешочек, в котором находится его награда за идентификацию Иисуса. </font></br>
+			<font class="smalltext">3. Загадка картины - кто же находится справа от Христа - Магдалина или Иоанн (интересный факт - этот герой полное зеркальное отражение Христа). </font></br>
+			<font class="smalltext">4. Апостол Петр с ножом в руке (им он позже отрезал ухо солдату, пытающемуся арестовать Иисуса).</font>
+		</div>
+		</td>
+		<td align=right>
+		<div align=left width=450px>
+			<font class="smalltext">5. 12 апостолов сгруппированы в 4 группы по 3 человека, и есть также 3 окна. Цифра три- ссылка на Святую Троицу.</font></br>
+			<font class="smalltext">6. Еще одна загадка картины - какая рыба лежит на столе: угорь - символизирует веру в Иисуса, а сельдь, напротив, символизирует неверующего. </font></br>
+			<font class="smalltext">7. Христос говорит: «Кто опускает руку со мной на блюдо, тот и предаст меня». Вместе с Христом одновременно свою руку к блюдцу на столе тянет Иуда. </font>
+		</div>
+		</td>
+	</tr>
+</table>
+<table align = center  width = 1020px>
+<tr height=60px bgcolor=#75675e>
+		<td>
+		</td>
+	</tr>
+	</table>
+</body>
+</html>
